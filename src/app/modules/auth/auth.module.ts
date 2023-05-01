8af80bbd4e6d2fafee12d9c094ee4c7002c9cb33
JAdrianP <jorgeadrianpages98@gmail.com>
2023-05-01 21:16:52 +0200

    creamos un formualrio con ngModel y lo validamos

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 
 
--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -5,6 +5,59 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
+
+
 export class LoginComponent {
+
+  public loginForm!:{
+    email: {
+      val:string;
+      error:string;
+      isValid: () => boolean;
+    },
+    password: {
+      val:string;
+      error:string;
+      isValid: () => boolean;
+    },
+  }
+
+constructor(){
+
+  this.loginForm ={ 
+    email:{
+      val: '',
+      error: 'el email es requerido',
+      isValid (){
+        const pattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]| \\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?| \[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+       
+        const isValidEmail = pattern.test(this.val);
+
+        if(this.val === '')
+        {
+          this.error = 'el email es requerido';
+        }
+        else{
+          this.error = 'el email no es valido';
+        }
+        return pattern.test(this.val);
+      }
+    },
+    password:{
+      val:'',
+      error:'El password es reequerido',
+      isValid(){
+        return this.val.length > 0
+      }
+    }
+  }
+}
+
+isValidForm(){
+  console.log(this.loginForm.email.isValid()  && this.loginForm.password.isValid() );
+  
+  return (this.loginForm.email.isValid()  && this.loginForm.password.isValid() )
+}
 
 }
