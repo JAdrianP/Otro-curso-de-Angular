87a3d4df4028668b1792aaba3f3d1b6c1a675f84
JAdrianP <jorgeadrianpages98@gmail.com>
2023-05-04 01:25:08 +0200

    otro ejemplo de reactive forms
    ahora mostramos los errores solo despues de pulsar iniciar sesion

--- src/app/modules/auth/login/login-form/login-form.component.ts
+++ src/app/modules/auth/login/login-form/login-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CONST_LOGIN_PAGE } from 'src/app/data/constants/pages/login/login.const';
 
@@ -7,20 +7,32 @@ import { CONST_LOGIN_PAGE } from 'src/app/data/constants/pages/login/login.const
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
 
-public loginForm: FormGroup;
+  public loginForm: FormGroup;
+  public loginSubmited = false;
 
-
-constructor(private formBuilder : FormBuilder){
-  this.loginForm = this.formBuilder.group({
-    email:['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
-    password:['', [Validators.required, Validators.maxLength(10)]]
-  });
-}
-  authenticate(){
-    console.log('authenticate');
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['me cago en diuuuu', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+      password: ['', [Validators.required, Validators.maxLength(10)]]
+    });
+  }
+  ngOnInit(): void {
+    //ESTO SE PONE TRAS PILLAR LOS DATOS DEL BACKEND
+    this.loginForm.get('email')?.setValue('nuevo@email.com');
+  }
+  authenticate() {
+    console.log(this.loginForm.value);
+    console.log(this.loginForm.get('password')!.value);
     
+    this.loginSubmited = true;
+    if (!this.loginSubmited) {
+      return;
+    }
+
+    console.log('authenticate');
+
   }
 
 }
