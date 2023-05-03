8467cd9ccaf1e49c798f08691d45962927f73b5f
JAdrianP <jorgeadrianpages98@gmail.com>
2023-05-04 01:08:34 +0200

    primer ejemplo de reactive forms
    en este ejemplo los mensajes de error aparecen sin necesidad de dar a iniciar sesion

--- src/app/modules/auth/login/login-form/login-form.component.ts
+++ src/app/modules/auth/login/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CONST_LOGIN_PAGE } from 'src/app/data/constants/pages/login/login.const';
 
 @Component({
@@ -8,19 +9,15 @@ import { CONST_LOGIN_PAGE } from 'src/app/data/constants/pages/login/login.const
 })
 export class LoginFormComponent {
 
+public loginForm: FormGroup;
 
-  public data = CONST_LOGIN_PAGE;
-  public loginForm;
 
-  constructor() {
-    this.loginForm = this.data.FORM;
-  }
-
-  isValidForm(){
-    console.log(this.loginForm.email.isValid()  && this.loginForm.password.isValid() );
-    
-    return (this.loginForm.email.isValid()  && this.loginForm.password.isValid() )
-  }
+constructor(private formBuilder : FormBuilder){
+  this.loginForm = this.formBuilder.group({
+    email:['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+    password:['', [Validators.required, Validators.maxLength(10)]]
+  });
+}
   authenticate(){
     console.log('authenticate');
     
